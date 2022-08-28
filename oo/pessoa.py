8b4a93d176f7a07edfa77f1e266c9c50ef1a6e4d
Mateus-Dev <Taldoindividuo@gmail.com>
2022-08-28 02:05:47 -0300

    Atualizacao do Projeto Python

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=19):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-if __name__ == '__main__':
-        p = Pessoa()
-        print(Pessoa.cumprimentar(p))
-        print(id(p))
-        print(p.cumprimentar())
 
+if __name__ == '__main__':
+    p = Pessoa('Mateus')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
         
