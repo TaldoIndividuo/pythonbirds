046887283ebd528e68a519aa66ba28b4b5b99579
Mateus-Dev <Taldoindividuo@gmail.com>
2022-08-28 02:31:00 -0300

    Criado Atributo Complexodo  Projeto Python

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=19):
+    def __init__(self, *filhos, nome=None, idade=19):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Mateus')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Renzo'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Arthur Miguel')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+
         
